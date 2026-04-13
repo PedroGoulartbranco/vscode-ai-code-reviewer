abb6d01b13792ee2e371d50470504fa9f837e678
Pedro Goulart Branco <pgoulartbranco@gmail.com>
2026-04-13 18:16:36 -0300

    testing new command bridge

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,17 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from 'vscode'; //Importa a "biblioteca" do VS Code.
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "ai-code-reviewer" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('ai-code-reviewer.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
+	console.log('ta funcionando seu lixo');
+
+	//Cria a função e da o nome
+	const mostrar_mensagem = vscode.commands.registerCommand('teste', () => { 
+
 		vscode.window.showInformationMessage('Hello World from ai-code-reviewer!');
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(mostrar_mensagem);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
