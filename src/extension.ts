0e5a48156bcd8489b03d043ffc97b7334d10fa69
Pedro Goulart Branco <pgoulartbranco@gmail.com>
2026-04-14 17:20:29 -0300

    change key validation to return object

--- src/extension.ts
+++ src/extension.ts
@@ -2,14 +2,15 @@ import * as vscode from 'vscode'; //Importa a "biblioteca" do VS Code.
 import { pedirInputAoUsuario } from './utils';
 
 export function activate(context: vscode.ExtensionContext) {
+	let descartavel = vscode.commands.registerCommand('ai-code-reviewer', async () => {
+		console.log('ta funcionando seu lixo');
 
+		//Retorna uma lista [chave, True ou False]
+		const lista_resposta_chave_e_booleano = pedirInputAoUsuario();
+		//vscode.window.showInformationMessage('');
+	}
 
-	console.log('ta funcionando seu lixo');
-
-	//Cria a função e da o nome
-	const mostrar_mensagem = pedirInputAoUsuario();
-	//vscode.window.showInformationMessage('');
-	
+);
 }
 
 export function deactivate() {}
--- src/utils.ts
+++ src/utils.ts
@@ -5,11 +5,25 @@ export async function pedirInputAoUsuario() {
         placeHolder: "Insira sua chave API do Gemini",
         prompt: "O que você quer perguntar?"
     });
-    let chave_real = verifiar_chave(String(input));
-    return input;
+    if (!input) {
+        return {
+            "chave": "",
+            "valido": false
+        };
+    }
+    let chave_correta = verifiar_chave(String(input));
+    return {
+        "chave": input,
+        "valido": chave_correta
+    };
 }
 
-async function verifiar_chave(chave: string) {
+function verifiar_chave(chave: string) {
     const formato_chave_api = /^AIza[0-9A-Za-z\-_]{35}$/;
     return formato_chave_api.test(chave);
+}
+
+function pegar_chave_json() {
+    const config = vscode.workspace.getConfiguration('aiReviewer.apiKey');
+    const chaveSalva = config.get<string>('apiKey');
 }
