ac50e9d49a630ee48b703206d6e3b6bc0ffac79f
Pedro Goulart Branco <pgoulartbranco@gmail.com>
2026-04-21 11:09:31 -0300

    remove redundant if-else logic and improve scalability

--- src/engine.ts
+++ src/engine.ts
@@ -4,71 +4,33 @@ import {  mostrar_revisao} from './ui-utils';
 import { verifiar_chave, pedirInputAoUsuario } from './utils';
 
 export async function decidir_modelo_de_resposta(nome_arquivo: string, linguagem: string, codigo: string, gemini: Gemini_Bot) {
-    if (linguagem === "html") {
+    try {
         await vscode.window.withProgress({
             location: vscode.ProgressLocation.Notification, 
             title: "Gemini Code Reviewer",
             cancellable: false 
         }, async (progress) => {
-            progress.report({ message: "Gerando revisão do HTML..." });
+            progress.report({ message: `Gerando revisão ${linguagem}...` });
             try {
-                let json_revisao_codigo = await gemini.gerar_revisao_html(codigo, nome_arquivo);
+                let json_revisao_codigo = {};
+                if (linguagem === 'html') {
+                     json_revisao_codigo = await gemini.gerar_revisao_html(codigo, nome_arquivo);
+                } else if (linguagem === 'css'){
+                     json_revisao_codigo = await gemini.gerar_revisao_css(codigo, nome_arquivo);
+                } else if (linguagem === 'python') {
+                     json_revisao_codigo = await gemini.gerar_revisao_python(codigo, nome_arquivo);
+                } else if (linguagem === 'javascript') {
+                     json_revisao_codigo = await gemini.gerar_revisao_javascript(codigo, nome_arquivo);
+                }
                 
-                mostrar_revisao(json_revisao_codigo, "html");
+                mostrar_revisao(json_revisao_codigo, linguagem);
                 
             } catch (erro) {
                 vscode.window.showErrorMessage("Falha ao gerar revisão.");
             }
         });
-    } else if (linguagem === "css") {
-        await vscode.window.withProgress({
-            location: vscode.ProgressLocation.Notification, 
-            title: "Gemini Code Reviewer",
-            cancellable: false 
-        }, async (progress) => {
-            progress.report({ message: "Gerando revisão do CSS..." });
-            try {
-                let json_revisao_codigo = await gemini.gerar_revisao_css(codigo, nome_arquivo);
-                
-                mostrar_revisao(json_revisao_codigo, "css");
-                
-            } catch (erro) {
-                vscode.window.showErrorMessage("Falha ao gerar revisão.");
-            }
-        });
-    }
-    else if (linguagem === "python") {
-        await vscode.window.withProgress({
-            location: vscode.ProgressLocation.Notification, 
-            title: "Gemini Code Reviewer",
-            cancellable: false 
-        }, async (progress) => {
-            progress.report({ message: "Gerando revisão do PYTHON..." });
-            try {
-                let json_revisao_codigo = await gemini.gerar_revisao_python(codigo, nome_arquivo);
-                
-                mostrar_revisao(json_revisao_codigo, "python");
-                
-            } catch (erro) {
-                vscode.window.showErrorMessage("Falha ao gerar revisão.");
-            }
-        });
-    }
-    else if (linguagem === "javascript") {
-        await vscode.window.withProgress({
-            location: vscode.ProgressLocation.Notification, 
-            title: "Gemini Code Reviewer",
-            cancellable: false 
-        }, async (progress) => {
-            progress.report({ message: "Gerando revisão do PYTHON..." });
-            try {
-                let json_revisao_codigo = await gemini.gerar_revisao_javascript(codigo, nome_arquivo);
-                
-                mostrar_revisao(json_revisao_codigo, "javascript");
-                
-            } catch (erro) {
-                vscode.window.showErrorMessage("Falha ao gerar revisão.");
-            }
-        });
+    } 
+    catch (erro) {
+        vscode.window.showErrorMessage("Falha ao gerar revisão.");
     }
 }
